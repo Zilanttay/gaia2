c42ae67a426768d5fc802a1cc5b6c0acf571fca6
Zilant <96441381+Zilanttay@users.noreply.github.com>
2024-08-13 18:16:53 +0300

    Add files via upload

--- gaian.js
+++ gaian.js
@@ -1,11 +1,20 @@
 const axios = require("axios");
 const fs = require('fs');
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const getRandomDelay = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getCurrentMoscowTime = () => {
+    const date = new Date();
+    // Получаем время UTC+3
+    const moscowTime = new Date(date.getTime() + 3 * 60 * 60 * 1000);
+    return moscowTime.toISOString().replace('T', ' ').substring(0, 19);
+};
 
 ;(async () => {
     try {
-        console.log('BOT Auto SendCHAT GAIAN By [Peking404XYogiPrt666]\n\n');
+        console.log('Goyda Net By [Cryptohomo Industries]\n\n');
         const addressList = await fs.readFileSync('keyword.txt', 'utf-8');
         const addressListArray = await addressList.split('\n');
 
@@ -37,8 +46,15 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
                 );
 
                 console.log("Response: [" + response.data.choices[0].message.content + "]\n");
-                console.log("WAIT 1 MENIT \n\n");
-                await delay(60000);
+                
+                // Логирование времени отправки сообщения
+                console.log("Last message sent at (MSK, UTC+3): " + getCurrentMoscowTime() + "\n");
+
+                console.log("Wait random time \n\n");
+
+                const randomDelay = getRandomDelay(10000, 65000);
+                await delay(randomDelay);
+
             } catch (postError) {
                 console.error("Error during axios post: ", postError);
             }
